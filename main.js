3348056ddd804cf49700b77544ba39032283ce8c
shixiang <shixiangcool@163.com>
2018-05-07 23:55:28 +0800

    update topNavBar

---
+++ main.js
@@ -0,0 +1,8 @@
+let topBar = document.querySelector('.topNavBar')
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 0) {
+    topBar.classList.add('active')
+  } else {
+    topBar.classList.remove('active')
+  }
+})
